0b1ceb3494fc873acb795ec7ded4bf9e6264b0de
ChWasam <wasam.developer@gmail.com>
2024-04-04 17:03:10 +0500

    Done with the basic setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,17 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
 
+// ["./src/**/*.{html,js}"]
+// ./ current directory
+// src aus me jo src ka folder ha
+// ** aus me jitna bhi folders han
+//*.{html,js}  aus me jtni bhi files han html han js
+
+//Un sabhi ko me conent mano ga mera
+// aus  ke basis pa me css generate kar do ga
+
+//output .css yeh brouser ka defaults ko hata data ha
+// browser flushing css
+// ta kah app ka look similar raha chrome me aur baqi sab browsers me
